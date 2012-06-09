18ef4108cfff44da29d271455ad2f93a39fa3d46
Kode Charlie <kodecharlie@yahoo.com>
2012-06-09 13:31:57 -0500

    Initial Commit

---
+++ TeaAnytime/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  TeaAnytime
+//
+//  Created by Kode Charlie on 6/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
